a5b1ed6a6650dfdf022eccea8de8f34f999bedd6
alga7396 <alga7396@pecule.co>
2024-12-16 13:22:31 +0100

    merge stonies

--- src/class/Component.php
+++ src/class/Component.php
@@ -6,6 +6,15 @@ use Illuminate\View\Component as ViewComponent;
 
 abstract class Component extends ViewComponent
 {
+    public $id;
 
+    public function createId() {
+        $this->id = $this->generateUniqueId();
+    }
+
+    protected function generateUniqueId($length = 10)
+    {
+        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+    }
 }
 
